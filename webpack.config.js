397c865985f51211d10a301b1c7cb1bac1a41ba0
simonscheurer <45239119+schueri@users.noreply.github.com>
2020-09-18 14:22:16 +0200

    Prepared project before start
    Added webpack, babel and first files (empty) to start with project structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,37 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: [
+        './src/js/index.js'
+    ],
     output: {
-        path: path.resolve(__dirname, 'dist/js'), // use node module path to resolve the __dirname == current path to an absolute path and combine it with dist/js
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'), // use node module path to resolve the __dirname == current path to an absolute path and combine it with dist/js
+        filename: 'js/bundle.js'
     },
-    mode: 'development' // as fast as possible, no compression
+    plugins: [
+        // Use src file and copy to dist folder
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./src/index.html"     
+        })
+    ],
+    module: {
+        // Required for babel
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    // Dev server does *not* create the files on disk
+    // It only streams then and creates on the fly. It does
+    // the same as a dev build, but without touching files
+    devServer: {
+        contentBase: './dist' // needs to point to the output path - otherwise it wont work
+    }
 };
